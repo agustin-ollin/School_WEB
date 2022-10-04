21980a4d58500217e0f5afce11f861e0c0a51cb4
Agustín Ollín García González <ollingonzalez37@gmail.com>
2022-10-04 18:26:32 -0500

    Validaciones en JavaScript para formulario de página de inicio

--- assets/js/auth/login.js
+++ assets/js/auth/login.js
@@ -1,30 +1,69 @@
-//var documentDom = document.addEventListener("DOMContentLoaded");
+// Expresión regular para validar Email
+var regExpEmail =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+// Expresión regular para validar Password
+var regExpPass = /^(?=.*\d)(?=.*[!@#$%^&*_])(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
-// Expresión regular
-var validEmail =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
+// Formulario
 var form = document.getElementById("form_login");
+// Botón para acceder
 var btnSubmit = document.getElementById("enviar")
+
+// EventListener para iniciar sesión
 btnSubmit.addEventListener('click', function(){
     validarFormulario();
 });
 
+/**
+ * Función para comprobar los valores ingresados por el usuario
+ */
 function validarFormulario(){
     var user = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-    if(validEmail.test(user) ){
-        if (user.length == 0) {
-            alert("No se ha registrado el usuario");
-        } else {
-            var password = document.getElementById("password").value;
-            if (password.length < 6) {
-                alert("La clave no es válida");
-            } else {
-                alert("Iniciando sesión!");
+    if (comprobarDatosRegistrados(user, "Email") && comprobarDatosRegistrados(password, "Contraseña")) {
+        if (comprobarEmail(user)){
+            if (comprobarPassword(password)) {
+                alert("Iniciando Sesión")
             }
         }
-    }else{
-        alert('Email invalido');
     }
-    //this.submit()
+}
+
+/**
+ * Función que ocupa expresión regular para comprobar que el texto ingresado sea un correo electrónico
+ * @param email Correo a comprobar
+ * @returns {boolean} Valor de retorno booleano
+ */
+function comprobarEmail(email){
+    if (!regExpEmail.test(email)) {
+        alert("Correo electrónico inválido")
+        return false
+    } else return true
+}
+
+/**
+ * Función que ocupa expresión regular para comprobar que la contraseña ingresada cumpla con:
+ * -Mínimo 6 caráteres
+ * -Al menos un símbolo
+ * -Letras mayúsculas y minúsculas
+ * -Un número
+ * @param password
+ */
+function comprobarPassword(password){
+    if (!regExpPass.test(password)){
+        alert("La contraseña no es válida debe contener: \n-Mínimo 6 caráteres\n-Al menos un símbolo(!@#$%^&*_)\n-Letras mayúsculas y minúsculas\n-Un número")
+        return false
+    } else return true
+}
+
+/**
+ * Función para validar que se haya ingresado texto en un input de acuerdo al contexto
+ * @param data Valor a comprobar
+ * @returns {boolean} Valor de retorno booleano
+ */
+function comprobarDatosRegistrados(data, tipo){
+    if (data.length == 0) {
+        alert("Error, Favor de registrar: " + tipo)
+        return false
+    } else return true
 }
